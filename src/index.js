c323c3319df94e4516e3444313560803889a10c1
Svetlana Vrublevskaya <svetulkavrub@gmail.com>
2020-10-02 23:55:06 +0300

    feat: implement max, min and avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (array && array.length > 0) {
+        return array.reduce((a, b) => Math.min(a, b));
+    } else {
+        return 0;
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array && array.length > 0) {
+        return array.reduce((a, b) => Math.max(a, b));
+    } else {
+        return 0;
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array && array.length > 0) {
+        return array.reduce((a, b) => (a + b)) / array.length;
+    } else {
+
+        return 0;
+    }
 }
